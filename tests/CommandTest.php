483d54f801a61828802f9327b9abae7f23b4bd6d
Benedict Pfisterer <pfisterer.bene@gmail.com>
2016-02-18 11:50:30 +0100

    add boilerplate

---
+++ tests/CommandTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Testing;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+abstract class CommandTest extends \PHPUnit_Framework_TestCase
+{
+    
+}
